5a3af4cb919e3a46eb224fc1c50e6e2c393d4086
Andrea Calvo <andrea.calvo@fgupm.upm.es>
2021-06-15 13:11:42 +0200

    Create app components

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 style={{textAlign: "center"}}> AUTHENTICATION & AUTHORIZATION WITH REACT </h1>
+        </div>
+    );
+};
+
+export default Home;
