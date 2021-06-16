295be905091a0a4a35df17036c62353d072720df
Andrea Calvo <andrea.calvo@fgupm.upm.es>
2021-06-16 14:26:00 +0200

    Implement the simulated server to run the back-end #3

---
+++ back-end-simulator/server.js
@@ -0,0 +1,51 @@
+const bodyParser = require('body-parser');
+const jsonServer = require('json-server');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const userDB = require('./db.json');
+
+const server = jsonServer.create()
+
+server.use(jsonServer.defaults());
+const SECRET_KEY = '123456789'
+const expiresIn = '1h'
+
+server.options('*', cors());
+server.use(cors());
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
+server.use(jsonServer.defaults());
+
+// Create a token from a payload
+function createToken(payload) {
+    return jwt.sign(payload, SECRET_KEY, {expiresIn})
+}
+
+// Check if the user exists in database
+function isAuthenticated({email, password}) {
+    return userDB.users.findIndex(user => user.email === email && user.password === password) !== -1
+}
+
+server.post('/signin', (req, res) => {
+    const {email, password} = req.body
+    console.log(email, password);
+    let a = isAuthenticated({email, password})
+    console.log(a);
+
+    // if (isAuthenticated({email, password}) === false) {
+    //     const status = 401
+    //     const message = 'Incorrect email or password'
+    //     res.status(status).json({status, message})
+    //     return
+    // }
+    const access_token = createToken({email, password})
+    console.log("Access Token:" + access_token);
+    res.status(200).json({access_token})
+})
+
+server.listen(8000, () => {
+    console.log('Run Auth API Server')
+    console.log("Node Server at http://localhost:" + 8000);
+    console.log("Hour: " + Date());
+})
+
