fa7fe3a3dcbe1f27acb6d09d5a78b8a98966a043
Andrea Calvo <andrea.calvo@fgupm.upm.es>
2021-06-16 08:54:12 +0200

    Styles for Sign In form #1

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,28 +1,18 @@
 import React from 'react';
 import {useForm} from "react-hook-form";
+import './SignIn.css'
 
 const SignIn = () => {
     const {register, handleSubmit, formState: {errors}} = useForm();
     const onSubmit = data => console.log('hola', data);
 
     return (
-        <div className="row">
-            <div className="col-md-6 offset-md-3">
+
+        <div className="card col-md-6 offset-md-3">
+            <div className="card-body col-md-10 offset-md-1">
 
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <h3 className="h3 mb-3 fw-normal">Sign In</h3>
-
-                    <div>
-                        <label>Username</label>
-                        <input className="form-control"
-                               {...register("username", {required: true, minLength: 3, maxLength: 20})}/>
-                        <p style={{color: "red"}}>{errors.username && errors.username.type === "required" &&
-                        <span>Required field</span>}</p>
-                        <p style={{color: "red"}}>{errors.username && errors.username.type === "minLength" &&
-                        <span>Min 3 chars</span>}</p>
-                        <p style={{color: "red"}}>{errors.username && errors.username.type === "maxLength" &&
-                        <span>Max 20 chars</span>}</p>
-                    </div>
 
                     <div>
                         <label>Email</label>
@@ -32,9 +22,9 @@ const SignIn = () => {
                                    required: true,
                                    pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
                                })}/>
-                        <p style={{color: "red"}}>{errors.email && errors.email.type === "required" &&
+                        <p className="fieldError">{errors.email && errors.email.type === "required" &&
                         <span>Required field</span>}</p>
-                        <p style={{color: "red"}}>{errors.email && errors.email.type === "pattern" &&
+                        <p>{errors.email && errors.email.type === "pattern" &&
                         <span>Not a valid email</span>}</p>
                     </div>
 
@@ -43,16 +33,16 @@ const SignIn = () => {
                         <input className="form-control"
                                type="password"
                                {...register("password", {required: true, minLength: 6, maxLength: 20})}/>
-                        <p style={{color: "red"}}>{errors.password && errors.password.type === "required" &&
+                        <p>{errors.password && errors.password.type === "required" &&
                         <span>Required field</span>}</p>
-                        <p style={{color: "red"}}>{errors.password && errors.password.type === "minLength" &&
-                        <span>Min 3 chars</span>}</p>
-                        <p style={{color: "red"}}>{errors.password && errors.password.type === "maxLength" &&
+                        <p>{errors.password && errors.password.type === "minLength" &&
+                        <span>Min 6 chars</span>}</p>
+                        <p>{errors.password && errors.password.type === "maxLength" &&
                         <span>Max 20 chars</span>}</p>
                     </div>
 
-                    <div className="col-md-3">
-                        <button className="w-100 btn btn-lg btn-dark" type="submit">Sign in</button>
+                    <div className="col-md-4">
+                        <button className="w-100 btn btn-md btn-dark" type="submit">Sign in</button>
                     </div>
                 </form>
             </div>
