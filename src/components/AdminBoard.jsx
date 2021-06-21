16b7c9c14ccc1d2ca0718caed30d889594081f5c
Andrea Calvo <andrea.calvo@fgupm.upm.es>
2021-06-21 10:38:41 +0200

    Add info to Admin Board #6

--- src/components/AdminBoard.jsx
+++ src/components/AdminBoard.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const AdminBoard = () => {
+const AdminBoard = (props) => {
+    const loggedUser = props.loggedUser;
+
     return (
-        <h1>
-            This is admin board
-        </h1>
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">Welcome {loggedUser.email}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">Admin Board</h6>
+                <p className="card-text">Your role is {loggedUser.role}.</p>
+                <p className="card-text">Your token is {loggedUser.access_token}</p>
+            </div>
+        </div>
     );
 };
 
